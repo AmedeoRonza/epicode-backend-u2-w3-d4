963c9a9a55527dd6c137504b271bba44580db4c3
Amedeo Ronza <amedeoronza@gmail.com>
2024-03-14 16:41:39 +0100

    Aggiungere i file di progetto.

---
+++ Pizzeria/Pizzeria/Controllers/PizzaController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pizzeria.Models;
+
+namespace Pizzeria.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class PizzaController : Controller
+    {
+        private ModelDbContext db = new ModelDbContext();
+
+        // GET: Pizza
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View(db.Pizza.ToList());
+        }
+
+        // GET: Pizza/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pizza pizza = db.Pizza.Find(id);
+            if (pizza == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pizza);
+        }
+
+        // GET: Pizza/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pizza/Create
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdPizza,Nome,Foto,Prezzo,TempoConsegna,Descrizione")] Pizza pizza, HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Content/Img"), fileName);
+                    file.SaveAs(path);
+                    pizza.Foto = "/Content/Img/" + fileName;
+                }
+                else
+                {
+                    pizza.Foto = "/Content/Img/Default.jpg";
+                }
+
+                if (ModelState.IsValid)
+                {
+                    db.Pizza.Add(pizza);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Gestisci l'eccezione, registrandola o visualizzando un messaggio all'utente
+                Console.WriteLine("Errore durante il salvataggio del file: " + ex.Message);
+            }
+
+            // Se si arriva a questo punto, significa che c'è un errore nel modello o nel salvataggio
+            return View(pizza);
+        }
+
+
+
+        // GET: Pizza/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pizza pizza = db.Pizza.Find(id);
+            if (pizza == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pizza);
+        }
+
+        // POST: Pizza/Edit/5
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdPizza,Nome,Foto,Prezzo,TempoConsegna,Descrizione")] Pizza pizza)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pizza).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pizza);
+        }
+
+        // GET: Pizza/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pizza pizza = db.Pizza.Find(id);
+            if (pizza == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pizza);
+        }
+
+        // POST: Pizza/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pizza pizza = db.Pizza.Find(id);
+            db.Pizza.Remove(pizza);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+
